63610f15228d11517836c5230c52a58e27bfe40c
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-11-17 06:38:14 +0000

    [Finish] 4-22 ﾍﾟｰｼﾞﾈｰｼｮﾝの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
-    @post_images = @user.post_images
+    @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
