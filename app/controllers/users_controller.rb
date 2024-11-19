4debccc0f62022745a5657c165043c4f9b5820b6
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-11-19 07:16:35 +0000

    [Finish] 他のﾕｰｻﾞｰからのｱｸｾｽを制限しよう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(current_user.id)
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
@@ -19,4 +21,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
 end
